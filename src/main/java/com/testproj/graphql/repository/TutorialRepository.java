d951d522f80e2e3e71814b605de12138efe34ab4
Mahesh Peddi <maheshbabu.peddi@googlecloud.corp-partner.google.com>
2023-01-17 17:30:25 +0000

    updated with repository annotation

--- src/main/java/com/testproj/graphql/repository/TutorialRepository.java
+++ src/main/java/com/testproj/graphql/repository/TutorialRepository.java
@@ -2,6 +2,8 @@ package com.testproj.graphql.repository;
 
 import com.testproj.graphql.model.Tutorial;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
 }
